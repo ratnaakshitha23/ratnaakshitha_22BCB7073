198a1b31c38a0cea1fd2843aae3fe3b108022146
ratnaakshitha23 <akshitha.22bcb7073@vitapstudent.ac.in>
2025-06-17 18:06:31 +0530

    Delete Mongodb-project-(student_attendance_manager) directory

--- Mongodb-project-(student_attendance_manager)/models/Attendance.js
+++
@@ -1,9 +0,0 @@
-
-
-const mongoose = require('mongoose');
-const attendanceSchema = new mongoose.Schema({
-  studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student' },
-  date: String,
-  present: Boolean
-});
-module.exports = mongoose.model('Attendance', attendanceSchema);
